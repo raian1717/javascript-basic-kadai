89e4dc93fa5755b269a115a0a7c6de8fa48ffaca
raian1717 <166607649+raian1717@users.noreply.github.com>
2024-04-28 22:08:41 +0900

    Add files via upload

---
+++ kadai_005/kadai_007/calc.js
@@ -0,0 +1,23 @@
+let num
+
+num=15
+
+if (num % 15 == 0) {
+    console.log("numは3と5の倍数です")
+}
+
+else if (num % 3 == 0) {
+    console.log("numは3の倍数です")
+}
+
+else if (num % 5 == 0) {
+    console.log("numは5の倍数です")
+}
+
+else {
+    console.log(num)
+}
+
+
+
+
