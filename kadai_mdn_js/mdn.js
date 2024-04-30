89f4c0ec469bf9aa697cd7b16bfd8ab984fd781e
raian1717 <166607649+raian1717@users.noreply.github.com>
2024-04-30 13:58:44 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,15 @@
+
+let today = new Date ()
+
+console.log(today)
+
+
+let year = today.getFullYear()
+
+console.log(year)
+
+let month = today.getMonth()
+
+let day = today.getDay()
+
+console.log(year + "年" + month + "月" + day + "日")
