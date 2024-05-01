f4a1c4e5fc134b9b97d669a90b08d3fa37c684ec
raian1717 <166607649+raian1717@users.noreply.github.com>
2024-05-01 19:13:39 +0900

    Add files via upload

---
+++ kadai_009/holiday.js
@@ -0,0 +1,31 @@
+
+const  holidays = ["正月","成人の日","建国記念日","天皇誕生日","春分の日","昭和の日","憲法記念日","みどりの日","こどもの日","海の日","山の日","敬老の日","秋分の日","スポーツの日","文化の日","勤労感謝の日"];
+
+console.log(holidays)
+
+// for文
+
+let n
+
+for ( n = 0; n < 16; n++) {
+    console.log(holidays[n])
+    
+}
+
+// while文
+
+console.log(holidays.length)
+
+let i = 0
+
+while (i <= holidays.length) {
+    console.log(holidays[i])
+    i++
+}
+
+
+
+
+
+
+
